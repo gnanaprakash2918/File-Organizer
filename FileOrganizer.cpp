2f6e823f7044a28865c6ca875a3e2d530933028d
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-01-05 23:05:29 +0530

    Iterated over the cwd and stored files in binary format

--- FileOrganizer.cpp
+++ FileOrganizer.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <fstream>
 
 int main() {
     try {
+        // Get the Current Working Directory
         std::filesystem::path current_working_directory = std::filesystem::current_path();
-        std::cout << current_working_directory;
+
+        // Iterate over the current directory
+        for(const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator(current_working_directory)) {
+            if(entry.is_regular_file()) {
+                // Regular File (No directories or shortcut links)
+                std::cout << entry.path().string() << std::endl;
+
+                // Open the file in read mode
+                std::ifstream file(entry.path(), std::ios::binary);
+                if(file) {
+                    std::string file_content(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+                }
+            }
+        }
     }
      catch (const std::filesystem::filesystem_error &e) {
         std::cout << e.what();
