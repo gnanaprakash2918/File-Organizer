75ce563d146d5b56df10a3dda04f66a9f126672e
Gnana Prakash M <gnanaprakash2918@gmail.com>
2025-01-05 22:14:47 +0530

    Added Gitignore and obtained the current working directory path

--- FileOrganizer.cpp
+++ FileOrganizer.cpp
@@ -3,5 +3,11 @@
 #include <string>
 
 int main() {
-
+    try {
+        std::filesystem::path current_working_directory = std::filesystem::current_path();
+        std::cout << current_working_directory;
+    }
+     catch (const std::filesystem::filesystem_error &e) {
+        std::cout << e.what();
+     }
 }
